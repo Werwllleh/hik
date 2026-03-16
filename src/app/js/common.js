b195a576a7fa0cd42f0007ac55d2d89eaa004bb9
Alexey A <allvxm4@gmail.com>
2026-03-16 19:14:44 +0300

    product-detail st1

--- src/app/js/common.js
+++ src/app/js/common.js
@@ -6,3 +6,20 @@ window.addEventListener('load', function () {
     ScrollTrigger.refresh();
   }
 });
+
+const progressiveImages = document.querySelectorAll('[data-image="progressive"]');
+if (progressiveImages.length) {
+  console.log(progressiveImages)
+  progressiveImages.forEach(progressiveImage => {
+    const lowResImage = new Image()
+    lowResImage.src = progressiveImage.src
+    lowResImage.onload = function() {
+      progressiveImage.src = progressiveImage.getAttribute('data-src')
+      progressiveImage.style.filter = 'none'
+    }
+    lowResImage.onerror = function() {
+      progressiveImage.style.filter = 'none'
+    }
+  })
+}
+

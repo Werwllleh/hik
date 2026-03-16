ef24b2110c211791ac4e1abb30e48ba5f6c9d056
Alexey A <allvxm4@gmail.com>
2026-03-17 00:31:29 +0300

    product-detail st1

--- src/blocks/collapse/collapse.js
+++ src/blocks/collapse/collapse.js
@@ -11,7 +11,7 @@ if (collapseBlocks.length) {
       const collapseBlockData = item.querySelector('.collapse-data');
 
       if (item.classList.contains('active')) {
-        collapseBlockWrap.style.maxHeight = getElementHeight(collapseBlockData);
+        collapseBlockWrap.style.height = getElementHeight(collapseBlockData);
         item.classList.add('active');
       }
 
@@ -30,12 +30,27 @@ if (collapseBlocks.length) {
 
         items.forEach(i => {
           i.classList.remove('active');
-          i.querySelector('.collapse-wrap').style.maxHeight = '';
+          i.querySelector('.collapse-wrap').style.height = '';
         });
 
         if (!isActive) {
-          collapseBlockWrap.style.maxHeight = getElementHeight(collapseBlockData);
+          if (collapseBlock.closest('.product-configurator')) {
+            const productConfiguratorHeight = collapseBlock.closest('.product-configurator').offsetHeight;
+            const collapseBlockHeight =  collapseBlock.offsetHeight;
+
+            collapseBlockWrap.style.height = `${productConfiguratorHeight - collapseBlockHeight}px`;
+          } else {
+            collapseBlockWrap.style.height = getElementHeight(collapseBlockData);
+          }
+
+          collapseBlockWrap.addEventListener('transitionend', function handler() {
+            collapseBlockWrap.style.height = 'auto';
+
+            collapseBlockWrap.removeEventListener('transitionend', handler);
+          }, { once: true });
+
           item.classList.add('active');
+
         }
       })
     })
@@ -46,7 +61,7 @@ if (collapseBlocks.length) {
         const collapseStageInfo = collapseBlockActiveItem.querySelector('.collapse-wrap');
         const collapseStageList = collapseStageInfo?.querySelector('.collapse-data');
 
-        collapseStageInfo.style.maxHeight = getElementHeight(collapseStageList);
+        collapseStageInfo.style.height = getElementHeight(collapseStageList);
       }
     })
 

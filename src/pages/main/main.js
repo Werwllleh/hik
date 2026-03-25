d470644c5da026a179c78d348aff5059bffc150e
Alexey A <allvxm4@gmail.com>
2026-03-25 23:10:59 +0300

    animations && search start

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -25,3 +25,59 @@ if (heroSection) {
     };
   })
 }
+
+const popularItems = document.querySelectorAll('.section-popular__items .hover-slider');
+if (popularItems.length) {
+
+  let mm = gsap.matchMedia();
+
+  mm.add("(min-width: 768px)", () => {
+    const popularCards = gsap.utils.toArray('.section-popular__items .hover-slider');
+
+    popularCards.forEach((card) => {
+      const title = card.querySelector('.hover-slider__name');
+      const price = card.querySelector('.hover-slider__price');
+
+      const cardTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: card,
+          start: "top 85%",
+          toggleActions: "play none none none",
+        }
+      });
+
+      cardTl.to(card, {
+        y: 0,
+        opacity: 1,
+        duration: 0.4,
+        ease: 'sine.out'
+      }, 0);
+
+
+      if (title) {
+        cardTl.to(title, {
+          y: 0,
+          opacity: 1,
+          duration: 0.3,
+          ease: 'sine.out'
+        }, 0.15);
+      }
+
+      if (price) {
+        cardTl.to(price, {
+          y: 0,
+          opacity: 1,
+          duration: 0.3,
+          ease: 'sine.out'
+        }, 0.25);
+      }
+    });
+
+    return () => {
+      furnitureCards.forEach(card => {
+        const trigger = ScrollTrigger.getTriggerById(card);
+        trigger?.kill();
+      });
+    };
+  });
+}

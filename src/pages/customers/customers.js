3a780e8212e24b5ee2846a2ea2ad88c9f647f657
Alexey A <allvxm4@gmail.com>
2026-03-31 00:03:12 +0300

    customers pages st1

---
+++ src/pages/customers/customers.js
@@ -0,0 +1,21 @@
+const customersNavSwiper = document.querySelector('.customers-nav.swiper')
+if (customersNavSwiper) {
+  const slides = customersNavSwiper.querySelectorAll('.swiper-slide'); // замените на ваш селектор слайдов
+  let initialSlideIndex = 0;
+
+  slides.forEach((slide, index) => {
+    if (slide.classList.contains('swiper-slide-active')) {
+      initialSlideIndex = index;
+    }
+  });
+
+  new Swiper(customersNavSwiper, {
+    speed: 600,
+    slidesPerView: "auto",
+    initialSlide: initialSlideIndex,
+    navigation: {
+      nextEl: '.customers-nav__next',
+      prevEl: '.customers-nav__prev',
+    },
+  });
+}
